2ad8a56d2d409995550735077e3feca91e5808d8
bwarfield <itsabeejay@yahoo.com>
2015-03-18 13:10:01 -0700

    Signed-off-by: bwarfield <itsabeejay@yahoo.com>

---
+++ Project/ShootEmUp/ShootEmUp/src/edu/rcc/student/bwarfield/cis18b/ShootEmUp/Game.java
@@ -0,0 +1,13 @@
+
+package edu.rcc.student.bwarfield.cis18b.ShootEmUp;
+
+import java.awt.Canvas;
+
+//author Brian Warfield
+public class Game extends Canvas{
+    //game dimentions
+    private final int WIDTH = 800;
+    private final int HEIGHT = 600;
+    // True if the game is currently "running"
+    private boolean gameRunning = true;
+}
